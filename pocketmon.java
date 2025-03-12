09b4d32c5167f9493bd63f4f6551521d254ae94d
gracegoeun47@gmail.com <gracegoeun@naver.com>
2025-03-12 16:56:50 +0900

    예시 추가

--- pocketmon.java
+++ pocketmon.java
@@ -2,17 +2,24 @@
 package cote02;
 
 import java.util.HashSet;
+import java.util.Set;
 
 public class pocketmon {
     public static void main(String[] args) {
-        int[] nums = { 3, 1, 2, 3 };
-        int result = solution(nums);
-        System.out.println(result);
+        int[] nums1 = { 3, 1, 2, 3 };
+        System.out.println(solution(nums1));
+
+        int[] nums2 = { 3, 3, 3, 2, 2, 4 };
+        System.out.println(solution(nums2));
+
+        int[] nums3 = { 3, 3, 3, 2, 2, 2 };
+        System.out.println(solution(nums3));
+
     }
 
     public static int solution(int[] nums) {
         // Set을 이용하여 종류만 셀렉
-        HashSet<Integer> uniquePocketmons = new HashSet<>();
+        Set<Integer> uniquePocketmons = new HashSet<>();
         // nums 배열에서 포켓몬 종류 set에 추가
         for (int num : nums) {
             uniquePocketmons.add(num);
