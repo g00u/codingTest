8726c42e143cfdf260bcb4fdf70f5b9735b0d62f
gracegoeun47@gmail.com <gracegoeun@naver.com>
2025-03-13 00:05:21 +0900

    class name Solution

--- Solution.java
+++ Solution.java
@@ -4,19 +4,7 @@ package cote02;
 import java.util.HashSet;
 import java.util.Set;
 
-public class pocketmon {
-    public static void main(String[] args) {
-        int[] nums1 = { 3, 1, 2, 3 };
-        System.out.println(solution(nums1));
-
-        int[] nums2 = { 3, 3, 3, 2, 2, 4 };
-        System.out.println(solution(nums2));
-
-        int[] nums3 = { 3, 3, 3, 2, 2, 2 };
-        System.out.println(solution(nums3));
-
-    }
-
+public class Solution {
     public static int solution(int[] nums) {
         // Set을 이용하여 종류만 셀렉
         Set<Integer> uniquePocketmons = new HashSet<>();
